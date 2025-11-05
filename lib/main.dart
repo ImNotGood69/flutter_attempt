e9a22c7041ffe5f1bc3a5cc61ba1973b1680b8c1
Dominykas Sicas <Domsu60@gmail.com>
2025-11-05 14:05:20 +0000

    Commit because i got kicked out of private room at library

--- lib/main.dart
+++ lib/main.dart
@@ -6,18 +6,27 @@ void main() {
 
 class App extends StatelessWidget {
   const App({super.key});
-  
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'Sandwich Shop App',
       home: Scaffold(
         appBar: AppBar(title: const Text('Sandwich Counter')),
-        body: const Center(
-          child: Text('Welcome to my Sandwich Shop!'),
-        ),
+        body: const Center(child: Text('Welcome to my Sandwich Shop!')),
       ),
     );
+  }
+}
+
+class OrderItemDisplay extends StatelessWidget {
+  final String itemType;
+  final int quantity;
+
+  const OrderItemDisplay(this.quantity, this.itemType, {super.key});
+  @override
+  Widget build(BuildContext context) {
+    return Text('This is a placeholder for OrderItemDisplay');
   }
 }
 
